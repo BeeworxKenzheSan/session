474b95f6e30b7ad01ad9f65e39146d6d9cc7a944
Nursultan Kenzhebaev <nursultan@beeworx.de>
2024-07-18 14:43:59 +0600

    session

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function App() {
+  return <StyledDiv></StyledDiv>;
+}
+
+export default App;
+
+const StyledDiv = styled.div`
+  background-color: #676f76;
+`;
---
+++ src/components/UI/Circle.jsx
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+const CIRCLE_VARIANTS = {
+  default: "grey",
+  success: "green",
+  error: "red",
+  processing: "blue",
+  warning: "orange",
+};
+
+const getCicleBackground = (props) => {
+  const styles = CIRCLE_VARIANTS[props.variant];
+  if (!styles) {
+    return css`
+      background: ${CIRCLE_VARIANTS["default"]};
+    `;
+  }
+  return css`
+    background: ${styles};
+  `;
+};
+
+export const Circle = (props) => {
+  return <StyledCircle variant={props.variant} />;
+};
+
+const StyledCircle = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 5px solid white;
+  ${getCicleBackground};
+`;
